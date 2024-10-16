f4d73057545061f0d89e5166b3e41dfee2392bd7
hendrik-spl <145655111+hendrik-spl@users.noreply.github.com>
2024-10-16 15:09:29 +0200

    set up of initial database models

--- db/models.py
+++ db/models.py
@@ -1,33 +1,90 @@
-from sqlalchemy import Column, Integer, String, Date, Float
+from sqlalchemy import Column, Integer, String, Date, Float, ForeignKey, Boolean
+from sqlalchemy.orm import relationship
 from db_conn import Base
 
-class Full_Record(Base):
-    __tablename__ = 'full_records'
+class FactFunding(Base):
+    __tablename__ = 'fact_funding'
     
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    transaction_id = Column(Integer, primary_key=True)
+    organization_id = Column(Integer, ForeignKey('dimension_organization.organization_id'))
     transaction_name = Column(String)
     transaction_name_url = Column(String)
-    organization_industries = Column(String)
-    lead_investors = Column(String)
-    investor_names = Column(String)
     money_raised = Column(Float)
     money_raised_currency = Column(String)
-    money_raised_in_usd = Column(Float)
+    money_raised_usd = Column(Float)
     funding_type = Column(String)
     announced_date = Column(Date)
     pre_money_valuation = Column(Float)
     pre_money_valuation_currency = Column(String)
-    pre_money_valuation_in_usd = Column(Float)
-    organization_name = Column(String)
-    organization_name_url = Column(String)
-    organization_location = Column(String)
-    organization_description = Column(String)
-    organization_website = Column(String)
+    pre_money_valuation_usd = Column(Float)
     funding_stage = Column(String)
     number_of_funding_rounds = Column(Integer)
     total_funding_amount = Column(Float)
-    total_funding_amount_currency = Column(String)
-    total_funding_amount_in_usd = Column(Float)
-    equity_only_funding = Column(String)
+    total_funding_currency = Column(String)
+    total_funding_amount_usd = Column(Float)
+    equity_only = Column(Boolean)
     funding_status = Column(String)
     number_of_investors = Column(Float)
+
+    organization = relationship('DimensionOrganization', back_populates='fundings')
+    investors = relationship('InvestorMapping', back_populates='funding')
+
+class DimensionOrganization(Base):
+    __tablename__ = 'dimension_organization'
+    
+    organization_id = Column(Integer, primary_key=True)
+    organization_name = Column(String)
+    organization_website = Column(String)
+    organization_name_url = Column(String)
+    organization_description = Column(String)
+    location_id = Column(Integer, ForeignKey('dimension_location.location_id'))
+    
+    location = relationship('DimensionLocation', back_populates='organizations')
+    fundings = relationship('FactFunding', back_populates='organization')
+    industries = relationship('IndustryMapping', back_populates='organization')
+
+class DimensionLocation(Base):
+    __tablename__ = 'dimension_location'
+    
+    location_id = Column(Integer, primary_key=True)
+    city = Column(String)
+    region = Column(String)
+    country = Column(String)
+    continent = Column(String)
+
+    organizations = relationship('DimensionOrganization', back_populates='location')
+
+class DimensionInvestor(Base):
+    __tablename__ = 'dimension_investor'
+    
+    investor_id = Column(Integer, primary_key=True)
+    investor = Column(String)
+    
+    investments = relationship('InvestorMapping', back_populates='investor')
+
+class InvestorMapping(Base):
+    __tablename__ = 'investor_mapping'
+    
+    transaction_id = Column(Integer, ForeignKey('fact_funding.transaction_id'), primary_key=True)
+    investor_id = Column(Integer, ForeignKey('dimension_investor.investor_id'), primary_key=True)
+    is_lead_investor = Column(Boolean)
+
+    funding = relationship('FactFunding', back_populates='investors')
+    investor = relationship('DimensionInvestor', back_populates='investments')
+
+class DimensionIndustry(Base):
+    __tablename__ = 'dimension_industry'
+    
+    industry_id = Column(Integer, primary_key=True)
+    industry = Column(String)
+
+    organizations = relationship('IndustryMapping', back_populates='industry')
+
+class IndustryMapping(Base):
+    __tablename__ = 'industry_mapping'
+    
+    organization_id = Column(Integer, ForeignKey('dimension_organization.organization_id'), primary_key=True)
+    industry_id = Column(Integer, ForeignKey('dimension_industry.industry_id'), primary_key=True)
+
+    organization = relationship('DimensionOrganization', back_populates='industries')
+    industry = relationship('DimensionIndustry', back_populates='organizations')
